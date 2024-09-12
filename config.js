5a9cdab69554c43b5cb84fe9a3cda71689fe7c7b
KiyotakaX011 <142149939+KiyotakaX011@users.noreply.github.com>
2024-09-12 22:02:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
           logging: false,
         }),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '',
+  SUDO: process.env.SUDO || '918767585726,919520211154',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
